004075e07081b5265f53ea7e3946583801ef1281
Just Playin <jp@justplayingames.com>
2017-08-26 09:21:36 -0500

    Use app game container for game.  Improved game feel.

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,21 +1,10 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
-        <title>Canned Kitty!</title>
-        <script src="//cdn.jsdelivr.net/phaser/2.6.2/phaser.min.js"></script>
-        <style media="screen" type="text/css">
+@extends('layouts.app.game')
 
-            body {
-                margin: 0 !important
-            };
+@section('title')
+Canned Kitty!
+@endsection
 
-        </style>
-
-    </head>
-    <body>
-
+@section('js')
     <script type="text/javascript">
 
     window.onload = function() {
@@ -26,10 +15,10 @@
         var foreground_group = null;
 
         var game = new Phaser.Game(
-            window.innerWidth * window.devicePixelRatio,
-            window.innerHeight * window.devicePixelRatio,
-            Phaser.CANVAS,
-            '',
+            667,
+            375,
+            Phaser.AUTO,
+            'game',
             {
                 preload: preload,
                 create: create,
@@ -65,7 +54,8 @@
             game.load.image('trashbag1', 'img/trashbag1-122x150.png')
             game.load.image('trashbag2', 'img/trashbag2-122x150.png')
 
-            game.load.image('cat', 'img/kitty.png');
+            game.load.image('cat1', 'img/kitty.png');
+            game.load.image('cat2', 'img/kitty2.png');
 
             game.load.audio('meow_a', 'wav/meow_A5.wav');
             game.load.audio('meow_b', 'wav/meow_B5.wav');
@@ -74,11 +64,34 @@
             game.load.audio('meow_e', 'wav/meow_E5.wav');
             game.load.audio('meow_f', 'wav/meow_F5.wav');
             game.load.audio('meow_g', 'wav/meow_G5.wav');
+
+            game.load.audio('trashcan', 'wav/trashcan.wav');
         }
 
         function create () {
 
+            if (!this.game.device.desktop)
+            {
+                game.scale.forceOrientation(true, false);
+                game.scale.enterIncorrectOrientation.add(
+                    function () {
+                        document.getElementById('rotate').style.display = 'flex';
+                        document.getElementById('game').style.display = 'none';
+                        game.paused = true;
+                    }
+                );
+                game.scale.leaveIncorrectOrientation.add(
+                    function () {
+                        document.getElementById('game').style.display = 'flex';
+                        document.getElementById('rotate').style.display = 'none';
+                        game.paused = false;
+                    }
+                );
+            }
+
             game.stage.backgroundColor = '#000';
+
+            trashcan_audio = game.add.audio('trashcan');
 
             meows = [
                 game.add.audio('meow_a'), // 0
@@ -109,49 +122,104 @@
             song = 0;
             note = 0;
 
-            paths = [
+            cats = [
                 {
-                    from: {x: 100, y: 240},
-                    to: {x: 0, y: 240}
-                },
-                {
-                    from: {x: 100, y: 240},
-                    to: {x: 200, y: 240}
-                },
-                {
-                    from: {x: 100, y: 240},
-                    to: {x: 100, y: 100}
-                },
-                {
-                    from: {x: 250, y: 240},
-                    to: {x: 250, y: 120}
+                    image: 'cat1',
+                    anchor: {x: 0.5, y: 0},
+                    paths: [
+                        {
+                            from: {x: 100, y: 240},
+                            to: {x: 20, y: 240}
+                        },
+                        {
+                            from: {x: 100, y: 240},
+                            to: {x: 230, y: 240}
+                        },
+                        {
+                            from: {x: 170, y: 240},
+                            to: {x: 170, y: 100}
+                        },
+                        {
+                            from: {x: 300, y: 240},
+                            to: {x: 300, y: 100}
+                        },
+                        {
+                            from: {x: 290, y: 240},
+                            to: {x: 240, y: 240}
+                        },
+                        {
+                            from: {x: 400, y: 240},
+                            to: {x: 400, y: 140}
+                        },
+                        {
+                            from: {x: 540, y: 240},
+                            to: {x: 540, y: 100}
+                        }, 
+                        {
+                            from: {x: 540, y: 240},
+                            to: {x: 490, y: 240}
+                        },
+                        {
+                            from: {x: 550, y: 240},
+                            to: {x: 630, y: 240}
+                        }
+                    ]
                 },
 
                 {
-                    from: {x: 250, y: 240},
-                    to: {x: 200, y: 240}
-                },
-                {
-                    from: {x: 400, y: 240},
-                    to: {x: 400, y: 180}
-                },
+                    image: 'cat2',
+                    anchor: {x: 0.25, y: 0},
+                    paths: [
+                        {
+                            from: {x: 100, y: 240},
+                            to: {x: 20, y: 240}
+                        },
+                        {
+                            from: {x: 100, y: 240},
+                            to: {x: 230, y: 240}
+                        },
+                        {
+                            from: {x: 170, y: 240},
+                            to: {x: 170, y: 100}
+                        },
+                        {
+                            from: {x: 300, y: 240},
+                            to: {x: 300, y: 100}
+                        },
+                        {
+                            from: {x: 290, y: 240},
+                            to: {x: 240, y: 240}
+                        },
+                        {
+                            from: {x: 400, y: 240},
+                            to: {x: 400, y: 140}
+                        },
+                        {
+                            from: {x: 540, y: 240},
+                            to: {x: 540, y: 100}
+                        }, 
+                        {
+                            from: {x: 540, y: 240},
+                            to: {x: 490, y: 240}
+                        },
+                        {
+                            from: {x: 550, y: 240},
+                            to: {x: 630, y: 240}
+                        }
+                    ]
+                }
+            ]
 
-                {
-                    from: {x: 500, y: 240},
-                    to: {x: 500, y: 100}
-                },
+            createBackground();
 
-                {
-                    from: {x: 500, y: 240},
-                    to: {x: 450, y: 240}
-                },
+            cat_group = game.add.group();
+            foreground_group = game.add.group();
 
-                {
-                    from: {x: 500, y: 240},
-                    to: {x: 580, y: 240}
-                },
-            ];
+            create_trash();
+            create_cat();
+        }
 
+        function createBackground() {
             background_group = game.add.group();
 
             var background = background_group.create(
@@ -161,30 +229,19 @@
             );
 
             background.anchor.setTo(0, 0);
-
-
-            cat_group = game.add.group();
-
-            foreground_group = game.add.group();
-
-            var trash = [
-                foreground_group.create(100, 155, 'trashcan1'),
-                foreground_group.create(20, 225, 'trashbag2'),
-                foreground_group.create(360, 200, 'trashbag1'),
-                foreground_group.create(250, 170, 'trashcan2'),
-                foreground_group.create(480, 165, 'trashcan1'),
-            ];
-
-            create_cat();
-
         }
 
         function createText() {
+            createTitleText();
+            createScoreText();
+        }
+
+        function createTitleText() {
             titleText = game.add.text(
-                16, 16, 'Canned Kitty', {fontSize: '54px'});
+                16, 16, 'Canned Kitty', {fontSize: '54px'}
+            );
 
             titleText.font = 'Luckiest Guy';
-            //titleText.size = 72;
 
             grd = titleText.context.createLinearGradient(0, 0, 0, titleText.canvas.height);
             grd.addColorStop(0, '#FFF');
@@ -195,29 +252,55 @@
             titleText.stroke = '#000000';
             titleText.strokeThickness = 2;
             titleText.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
+        }
 
+        function createScoreText() {
             scoreText = game.add.text(500, 40, 'Score: 0', { fontSize: '32px', fill: '#FFF' });
             scoreText.font = 'Lobster'
             scoreText.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
         }
 
         function render() {
+            // render_debug();
+        }
+
+        function render_debug() {
+            game.debug.text( 'innerWidth: ' + window.innerWidth , 10, 100);
+            game.debug.text( 'innerHeight: ' + window.innerHeight , 10, 112);
+            game.debug.text( 'devicePixelRatio: ' + window.devicePixelRatio , 10, 124);
+            game.debug.text( 'isFullScreen: ' + game.scale.isFullScreen, 10, 136);
+            game.debug.text( 'scaleMode: ' + game.scale.scaleMode, 10, 148);
+
+            game.debug.text( 'camera.height: ' + game.camera.height, 10, 160);
+            game.debug.text( 'camera.width: ' + game.camera.width, 10, 172);
         }
 
         function create_cat() {
+        
+            cat = cats[Math.floor(Math.random() * cats.length)];
 
-            path = paths[Math.floor(Math.random() * paths.length)];
+            path = cat.paths[Math.floor(Math.random() * cat.paths.length)];
 
-            var cat = cat_group.create(path.from.x, path.from.y, 'cat');
+            var cat_sprite = cat_group.create(
+                path.from.x, 
+                path.from.y, 
+                cat.image
+            );
 
-            cat.inputEnabled = true;
+            cat_sprite.anchor = cat.anchor;
+            cat_sprite.scale.x = Phaser.Utils.randomChoice(-1.0, 1.0);
 
-            game.add.tween(cat).to( path.to, 4000, Phaser.Easing.Elastic.Out, true);
+            cat_sprite.inputEnabled = true;
 
-            cat.events.onInputDown.add(
+            game.add.tween(cat_sprite).to( 
+                path.to, 
+                4000, 
+                Phaser.Easing.Elastic.Out, 
+                true
+            );
+
+            cat_sprite.events.onInputDown.add(
                 function() {
-
-                    // meow = meows[Math.floor(Math.random() * meows.length)];
 
                     meow = meows[songs[song][note]];
                     note = note + 1
@@ -229,19 +312,100 @@
                     }
 
                     meow.play();
-                    cat.kill();
+                    cat_sprite.kill();
 
                     score += 1;
                     scoreText.text = 'Score: ' + score;
 
                     create_cat();
                 },
-                cat
+                cat_sprite
+            );
+        }
+
+        function create_trash()
+        {
+            var trash = [
+                foreground_group.create(100, 155, 'trashcan1'),
+                foreground_group.create(20, 225, 'trashbag2'),
+                foreground_group.create(360, 200, 'trashbag1'),
+                foreground_group.create(250, 170, 'trashcan2'),
+                foreground_group.create(490, 165, 'trashcan1'),
+            ];
+
+            trash[0].inputEnabled = true;
+            trash[0].tween = null;
+            trash[0].events.onInputDown.add(
+                function() {
+                    if (trash[0].tween) {
+                        trash[0].tween.stop();
+                        trash[0].tween = null;
+                        trash[0].position.set(100, 155);
+                    }
+
+                    trashcan_audio.play();
+                    trash[0].tween = game.add.tween(trash[0]).to( {x: 102, y: 155}, 50, Phaser.Easing.Elastic.Out, true, 0, 1, true);
+                }
+            );
+
+            trash[1].inputEnabled = true;
+            trash[1].tween = null;
+            trash[1].events.onInputDown.add(
+                function() {
+                    if (trash[1].tween) {
+                        trash[1].tween.stop();
+                        trash[1].tween = null;
+                        trash[1].position.set(20, 225);
+                    }
+
+                    trash[1].tween = game.add.tween(trash[1]).to( {x: 20, y: 227}, 50, Phaser.Easing.Elastic.Out, true, 0, 1, true);
+                }
+            );
+
+            trash[2].inputEnabled = true;
+            trash[2].tween = null;
+            trash[2].events.onInputDown.add(
+                function() {
+                    if (trash[2].tween) {
+                        trash[2].tween.stop();
+                        trash[2].tween = null;
+                        trash[2].position.set(360, 200);
+                    }
+
+                    trash[2].tween = game.add.tween(trash[2]).to( {x: 360, y: 202}, 50, Phaser.Easing.Elastic.Out, true, 0, 1, true);
+                }
+            );
+
+            trash[3].inputEnabled = true;
+            trash[3].tween = null;
+            trash[3].events.onInputDown.add(
+                function() {
+                    if (trash[3].tween) {
+                        trash[3].tween.stop();
+                        trash[3].tween = null;
+                        trash[3].position.set(250, 170);
+                    }
+
+                    trashcan_audio.play();
+                    trash[3].tween = game.add.tween(trash[3]).to( {x: 253, y: 170}, 50, Phaser.Easing.Elastic.Out, true, 0, 1, true);
+                }
+            );
+
+            trash[4].inputEnabled = true;
+            trash[4].tween = null;
+            trash[4].events.onInputDown.add(
+                function() {
+                    if (trash[4].tween) {
+                        trash[4].tween.stop();
+                        trash[4].tween = null;
+                        trash[4].position.set(490, 165);
+                    }
+
+                    trashcan_audio.play();
+                    trash[4].tween = game.add.tween(trash[4]).to( {x: 492, y: 165}, 50, Phaser.Easing.Elastic.Out, true, 0, 1, true);
+                }
             );
         }
     };
-
     </script>
-
-    </body>
-</html>
+@endsection
